8ff93b6704d8d3aff0c77b3ade729be64f3b1959
Brady Edwards <brady.edwards16@gmail.com>
2024-06-28 11:26:37 -0400

    RTWK Chapt 3 - BVH

--- hittable.h
+++ hittable.h
@@ -3,30 +3,34 @@
 
 #include "rtweekend.h"
 
+#include "aabb.h"
+
 class material;
 
 class hit_record {
-    public:
-        point3 p;
-        vec3 normal;
-        shared_ptr<material> mat;
-        double t;
-        bool front_face;
+public:
+    point3 p;
+    vec3 normal;
+    shared_ptr<material> mat;
+    double t;
+    bool front_face;
 
-        void set_face_normal(const ray& r, const vec3& outward_normal) {
-            // Sets the hit record normal vec
-            // NOTE: the parameter 'outward_normal' is assumed to have unit length
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        // Sets the hit record normal vec
+        // NOTE: the parameter 'outward_normal' is assumed to have unit length
 
-            front_face = dot(r.direction(), outward_normal) < 0;
-            normal = front_face ? outward_normal : -outward_normal;
-        }
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable {
-    public:
-        virtual ~hittable() = default;
+public:
+    virtual ~hittable() = default;
 
-        virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+    virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+
+    virtual aabb bounding_box() const = 0;
 };
 
 #endif
