504000e97edea1d71788a796890c44dda497a323
Brady Edwards <brady.edwards16@gmail.com>
2024-06-30 16:52:50 -0400

    RTW Chapt 4 - Texture Mapping

--- sphere.h
+++ sphere.h
@@ -50,6 +50,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        get_sphere_uv(outward_normal, rec.u, rec.v);
         rec.mat = mat;
 
         return true;
@@ -69,6 +70,14 @@ private:
         // Linearly interpolate from center1 to center2 according to time, where t=0
         // yields center1, and t=1 yields center 2
         return center1 + time*center_vec;
+    }
+
+    static void get_sphere_uv(const point3& p, double& u, double& v) {
+        auto theta = acos(-p.y());
+        auto phi   = atan2(-p.z(), p.x()) + pi;
+
+        u = phi / (2*pi);
+        v = theta / pi;
     }
 };
 
